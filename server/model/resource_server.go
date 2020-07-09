75f6c8ac8147d0f99b1532183cc897e8e8cdae7b
1032231418 <13725578065@163.com>
2020-07-09 10:29:42 +0800

    主机推送公钥

--- server/model/resource_server.go
+++ server/model/resource_server.go
@@ -7,7 +7,7 @@ type ResourceServer struct {
 	Port          int64       `json:"port"`   //SSH端口
 	User          string      `json:"user"`   //用户名
 	Pwd           string      `json:"pwd"`    //密码
-	Status        int         `json:"status"` //测试连接，1.未测试，2.连接中, 3.连接成功, 4. 连接异常
+	Status        int         `json:"status"` //测试连接，1.未测试，2.连接中, 3.连接成功, 4. 连接异常, 5.推送公钥成功，6。推送公钥失败
 	ResourceEnvId int         `json:"resource_env_id"`
 	ResourceEnv   ResourceEnv `json:"resourceenv" gorm:"ForeignKey:ResourceEnvId"`
 }
