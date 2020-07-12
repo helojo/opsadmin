d577789574b1e340d95e779407386b815eb096cb
1032231418 <13725578065@163.com>
2020-07-12 19:21:57 +0800

    项目提测

--- server/model/deploy_testing.go
+++ server/model/deploy_testing.go
@@ -10,16 +10,7 @@ type DeployTesting struct {
 	Files           string        `gorm:"type:longText" json:"files"`    // 同步的文件
 	Describe        string        `gorm:"type:longText" json:"describe"` // 描述
 	Status          int           `json:"status"`                        //0, 提测中, 1:提测成功 2:提测失败
+	Result          string        `gorm:"type:longText" json:"result"`   // 发布结果
 	DeployProjectId int           `json:"deploy_project_id"`
 	DeployProject   DeployProject `json:"deployproject" gorm:"ForeignKey:DeployProjectId"`
-}
-
-type DeployTestLog struct {
-	gorm.Model
-	Applicant       string        `json:"applicant"` // 申请人
-	Version         float64       `json:"version"`   // 提测版本
-	Tag             string        `json:"tag"`       // tag
-	Status          int           `json:"status"`    //1：成功 2：失败
-	DeployTestingId int           `json:"deploy_testing_id"`
-	DeployTesting   DeployTesting `json:"deploytesting" gorm:"ForeignKey:DeployTestingId"`
 }
